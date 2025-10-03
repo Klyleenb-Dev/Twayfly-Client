c24035a620a07d31b8ad14e1674e92eeefdc0c74
Klyleenb-Dev <klyleenb@gmail.com>
2025-10-02 21:46:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// abrir aviso quando clicar em entrar
 document.getElementById("openLoginBtn").addEventListener("click", () => {
   document.getElementById("loginUnavailable").classList.remove("hidden");
 });
 
+// fechar aviso
 document.getElementById("closeUnavailable").addEventListener("click", () => {
   document.getElementById("loginUnavailable").classList.add("hidden");
 });
